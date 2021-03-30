b755a15970cd96be8149e3cb37775e608a810d60
Ron Le <ronle9519@gmail.com>
2021-03-30 20:53:01 +0700

    New file

--- Task-4/main.js
+++ Task-4/main.js
@@ -5,12 +5,16 @@ function check(string){
 	return true;
 }
 function add(){
-	if (check(document.getElementById('username').value)){
-	 	document.getElementById('name').append(document.getElementById('username').value);
-	 	document.getElementById('age').append(document.getElementById('user-age').value);
-	 	document.getElementById('name').innerHTML = document.getElementById('name').innerHTML + '<br></br>';
-	 	document.getElementById('age').innerHTML = document.getElementById('age').innerHTML + '<br></br>';
+	if (document.getElementById('username').value !== ''){
+		if (check(document.getElementById('username').value) && Number(document.getElementById('user-age').value) > 0){
+	 		document.getElementById('name').append(document.getElementById('username').value);
+	 		document.getElementById('age').append(document.getElementById('user-age').value);
+	 		document.getElementById('name').innerHTML = document.getElementById('name').innerHTML + '<br></br>';
+	 		document.getElementById('age').innerHTML = document.getElementById('age').innerHTML + '<br></br>';
+		}else{
+			alert('Oops!!Something is wrong.');
+		}
 	}else{
-		alert('Oops!!Name doesn\'t include number.');
+		alert('Please Enter Your Full Name!!');
 	}
 }
