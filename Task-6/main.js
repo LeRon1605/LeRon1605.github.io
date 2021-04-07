16a9d4976ece3e158588b4242ec881399dca0a42
Ron Le <ronle9519@gmail.com>
2021-04-07 10:31:52 +0700

    Add

--- Task-6/main.js
+++ Task-6/main.js
@@ -1,16 +1,21 @@
 let tagUl = document.getElementsByTagName('ul');
 async function main(){
 	let information = await axios.get('https://6065d36cb8fbbd00175677e7.mockapi.io/s-group/users');
-	
 	for (x of information.data){
-		tagUl[0].innerHTML += '<li>' + 'name: ' + x['name'] + '    id: ' + x['id'] + '</li>';
+		tagUl[0].innerHTML += '<li>' + 'Name: ' + x['name'] + '- Id: ' + x['id'] + '</li>';
 	}
 }
 main();
 async function add(){
-	let newUser = {
-		'name': document.getElementById('username').value,
+	let userName = document.getElementById('username').value;
+	if (userName.trim() !== ''){
+		let newUser = {
+			'name': userName
+		}
+		let result = await axios.post('https://6065d36cb8fbbd00175677e7.mockapi.io/s-group/users',newUser);
+	}else{
+		alert('Invalid input');
 	}
-	let result = await axios.post('https://6065d36cb8fbbd00175677e7.mockapi.io/s-group/users',newUser);
+	
 	await main();
 }
