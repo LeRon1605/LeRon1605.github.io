322055d8720cb5f0b3beaee71b2a3393faf356da
Ron Le <ronle9519@gmail.com>
2021-04-07 19:24:45 +0700

    Fix bug

--- Task-4/main.js
+++ Task-4/main.js
@@ -5,11 +5,10 @@ function check(string){
 	return true;
 }
 function add(){
-	if (check(document.getElementById('username').value) && Number(document.getElementById('user-age').value) > 0){
+	if (check(document.getElementById('username').value) && Number(document.getElementById('user-age').value && document.getElementById('username').value.trim() !== '') > 0){
 	 	document.getElementById('name').append(document.getElementById('username').value);
 	 	document.getElementById('age').append(document.getElementById('user-age').value);
 	 	document.getElementById('name').innerHTML = document.getElementById('name').innerHTML + '<br></br>';
 	 	document.getElementById('age').innerHTML = document.getElementById('age').innerHTML + '<br></br>';
 	}else  $('#myModal').modal('show');
-	
 }
