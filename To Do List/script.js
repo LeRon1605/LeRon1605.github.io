7c7f87291abcd62d83748b0754ab941148b45a47
Ron Le <ronle9519@gmail.com>
2021-04-29 18:17:31 +0700

    Add

--- To Do List/script.js
+++ To Do List/script.js
@@ -1,41 +1,14 @@
-function changeColorNavBar(event){
-	event.style.color = '#3B44B0';
-	event.querySelector('span').style.display = 'block';
-	let pTag = document.querySelectorAll('#nav-bar .center p');
-	for (let i = 0;i < pTag.length;i++){
-		if (pTag[i] != event){
-			pTag[i].style.color = '#9A9EA1';
-			pTag[i].querySelector('span').style.display = 'none';
-		}
+
+let buttonList = document.getElementById('activeList');
+buttonList.onclick = function(){
+	document.getElementById('TypeList').style.display = 'block';
+	buttonList.onclick = function(){
+		document.getElementById('TypeList').style.display = 'none';
 	}
 }
-window.onload = function (){
-	let myImg = document.getElementById('carouselExampleFade');
-	let img1 = document.getElementById('intro-image1');
-	let img2 = document.getElementById('intro-image2');
-	let img3 = document.getElementById('intro-image3');
-	let img4 = document.getElementById('intro-image4');
-	myImg.style.transform = 'scale(1)';
-	img1.style.transform = 'scale(1)';
-	img2.style.transform = 'scale(1)';
-	img3.style.transform = 'scale(1)';
-	img4.style.transform = 'scale(1)';
-	let i = 0;
-	let text = 'Hello, My name is Rôn.<br>Welcome you to my Website.';
-	let brTagIndex = text.indexOf('<br>');
-	let codeIntroduce = document.getElementById('code-introduce');
-	function typeWriter(){
-		if (i < text.length){
-			if (i == brTagIndex) {
-				codeIntroduce.innerHTML += '<br>';
-				i += 4;
-			}else{
-				codeIntroduce.innerHTML += text[i];
-				i++;
-			}
-			setTimeout(typeWriter, 50);
-
-		}
+window.onload = function(){
+	let jobTags = document.getElementsByClassName('job');
+	for (tag of jobTags){
+		tag.style.transform = 'scale(1)';
 	}
-	typeWriter();
 }
